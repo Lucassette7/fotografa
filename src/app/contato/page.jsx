57b241f3a74d4df2d766ed807da30e59c71b47b9
LUCAS SETTE QUEIROZ <136368302+Lucassette7@users.noreply.github.com>
2025-07-16 15:42:34 -0300

    teste2

--- src/app/contato/page.jsx
+++ src/app/contato/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Navigation, Phone, Mail } from 'lucide-react';
+import axios from 'axios';
 
 const Contato = () => {
-  const numero = '5511966339734'   
+
+  const enviarMsgWhats = () => {
+    const numero = '5511966339734';
+    const mensagem = encodeURIComponent('Olá, tudo bem? Gostaria de um orçamento.');
+    alert(mensagem)
+    const url = `https://wa.me/${numero}?text=${mensagem}`;
+    window.open(url, '_blank');
+  };
+
   return (
   <section className="min-h-screen w-full bg-[#fdf6ec] text-[#5e4b3c] px-6 py-12 flex items-center justify-center">
     <div className="w-full max-w-4xl bg-white/80 rounded-2xl shadow-2xl p-8 md:p-12">
@@ -30,14 +39,11 @@ const Contato = () => {
           <p className="text-lg font-medium">
             Entre em contato pelo <span className="text-green-600">WhatsApp</span>
           </p>
-          <a
-            href={`https://wa.me/${numero}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block mt-2 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg transition"
+          <button className="inline-block mt-2 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg transition"
+            onClick={enviarMsgWhats}
           >
             Enviar Mensagem
-          </a>
+          </button>
         </div>
       </div>
     </div>
